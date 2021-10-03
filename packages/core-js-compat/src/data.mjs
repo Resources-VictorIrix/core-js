0d1fdb590c6e9eecfcbdf26e1601376ca16e30c3
Denis Pushkarev <zloirock@zloirock.ru>
2021-10-03 23:15:10 +0700

    add `Function.{ isCallable, isConstructor }` proposal
    https://github.com/caitp/TC39-Proposals/blob/trunk/tc39-reflect-isconstructor-iscallable.md

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -1495,6 +1495,10 @@ export const data = {
   },
   'esnext.composite-symbol': {
   },
+  'esnext.function.is-callable': {
+  },
+  'esnext.function.is-constructor': {
+  },
   // TODO: Remove from `core-js@4`
   'esnext.global-this': null,
   'esnext.iterator.constructor': {
